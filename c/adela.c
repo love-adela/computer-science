afaecf8d1a2b2bdf8d594e642e8ddefe6cb96c0e
love-adelar <love.adelar@gmail.com>
2020-12-31 16:01:28 +0900

    1주차, 2주차 실습

---
+++ c/adela.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int name(void) {
+  printf("adela");
+  return 0;
+}
---
+++ c/class-1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "adela.c"
+
+int main(void) {
+  printf("****************");
+  printf("이제 시작합니다.");
+  printf("이름: 정지윤");
+  return 0;
+}
