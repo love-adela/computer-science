ef27b608da56c79f2aa866db0294ac9626cc1216
love-adelar <love.adelar@gmail.com>
2021-01-16 19:47:39 +0900

    Add expression practice file

---
+++ c/class4/expression.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void) {
+  // 산술 연산자
+  int operand1 = 7;
+  int operand2 = 5;
+  int sum = operand1 + operand2;
+  int sub = operand1 - operand2;
+  int mul = operand1 * operand2;
+  int div = operand1 / operand2;
+
+  printf("%d+%d = %d\n", operand1, operand2, sum);
+  printf("%d-%d = %d\n", operand1, operand2, sub);
+  printf("%d*%d = %d\n", operand1, operand2, mul);
+  printf("%d/%d = %d\n", operand1, operand2, div);
+
+  double operand3 = 10 + 5;
+  printf("%f", a);
+
+
+  return 0;
+}
